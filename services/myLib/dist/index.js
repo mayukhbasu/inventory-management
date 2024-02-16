cf164285b34b369ce6431c5a393206f9dc36782c
Mayukh Basu <roxy.firebird@gmail.com>
2024-02-16 18:50:40 +0530

    basic kubernetes deployment

--- services/myLib/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = void 0;
-// packages/myLib/src/index.ts
-var greet = function (name) { return "Hello, ".concat(name, "!"); };
-exports.greet = greet;
--- services/myLib/src/index.ts
+++
@@ -1,2 +0,0 @@
-// packages/myLib/src/index.ts
-export const greet = (name: string): string => `Hello, ${name}!`;
