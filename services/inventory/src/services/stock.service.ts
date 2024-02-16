3b2fe1a6316994fa57dee83910934cedabe7ed08
Mayukh Basu <roxy.firebird@gmail.com>
2024-02-16 12:19:38 +0530

    code refactored

--- services/inventory/src/services/stock.service.ts
+++ services/inventory/src/services/stock.service.ts
@@ -1,26 +1,49 @@
+import { query } from "../config/db";
 import logger from "../logger";
-import { StockModel } from "../models/stock.model";
 
-export class StockService {
-  static async updateStock(productID: string, quantity: number, type: 'order' | 'receipt'): Promise<string> {
+type stockType = 'order' | 'receipt';
+
+type productStock = {
+  productID: string,
+  currentStock: number
+}
+
+export class StockService{
+  
+  static async updateStock(productID: string, quantity: number, type: stockType): Promise<string> {
+    logger.info('Inside update stock function of updateStock');
+    let sql = '';
+    if (type === 'order') {
+        sql = 'UPDATE StockLevels SET Quantity = Quantity - $2 WHERE ProductID = $1';
+        logger.debug(sql)
+    } else if (type === 'receipt') {
+        sql = 'UPDATE StockLevels SET Quantity = Quantity + $2 WHERE ProductID = $1';
+        logger.debug(sql);
+    }
+
     try {
-      const result = await StockModel.updateStock(productID, quantity, type);
-      return result;
+        const res = await query(sql, [productID, quantity]);
+        logger.error(res)
+        if (res.rowCount === 0) {
+            throw new Error('Product not found or quantity update failed.');
+        }
+        return `Stock updated for product ${productID}, quantity ${quantity}, type ${type}`;
     } catch (err) {
-        throw err;
+        throw new Error(`An error occurred: ${err}`);
     }
-  }
+}
 
-  static async getStockLevel(productID: string): Promise<{productID: string, currentStock: number}> {
-    logger.info('Started executing getStockLevel function inside StockService');
+  static async getStockLevel(productID: string): Promise<productStock> {
+    logger.info('Inside getStockLevel function of StockModel');
     try {
-      const result = await StockModel.getStockLevel(productID);
-      logger.info(result);
-      return result;
+      const result = await query('select quantity from stocklevels where productID = $1', [productID]);
+      const currentStock = result.rows[0].quantity;
+      logger.info(currentStock);
+      return {productID, currentStock}
     } catch(err) {
-      logger.error(err);
-      throw err;
+      logger.error(err)
+      throw new Error(`An error occurred: ${err}`);
     }
-
   }
+  
 }
